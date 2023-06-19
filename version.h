ed8e7bbad9ac1cd7c30ebb04c362932825ae1266
Ginakira <ginakira@foxmail.com>
2023-06-19 21:47:45 +0800

    feat: add clang-format config and version number

---
+++ version.h
@@ -0,0 +1,13 @@
+//
+// Created by Ginakira on 2023/6/19.
+//
+
+#ifndef SMARTSTAINER__VERSION_H_
+#define SMARTSTAINER__VERSION_H_
+
+#define MAJOR_VERSION 1
+#define MINOR_VERSION 1
+#define BUILD_VERSION 20230619
+#define APPLICATION_NAME "SmartStainer"
+
+#endif  // SMARTSTAINER__VERSION_H_
